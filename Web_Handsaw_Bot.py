9262a9d5e15757f250f40b3a8e5311b6260c592e
CiaranGodfrey <ciaran.laji.godfrey@cern.ch>
2018-08-29 17:42:22 +0200

    query logger name

--- Web_Handsaw_Bot.py
+++ Web_Handsaw_Bot.py
@@ -14,10 +14,12 @@ class SigTerm(SystemExit): pass
 def sigterm(sig,frm): raise SigTerm
 signal.signal(15,sigterm)
 
-def sendSlackMessage(alert):
-    print alert
+def sendSlackMessage(message, timestamp, logger):
+    print message
+    time = str(timestamp).split('.')[0]
     webhook_url = "WEBHOOK"
-    slack_data = {'text': "Testing new webHandsaw System ```%s```" % alert}
+    slack_data = {'text': """```%s at %s
+%s```""" %(logger, time, message)}
     response = requests.post(
         webhook_url, data=json.dumps(slack_data),
         headers={'Content-Type': 'application/json'}
@@ -29,23 +31,24 @@ def sendSlackMessage(alert):
         return True
 
 try:
-    database = "CONNECTION_STRING"
+    database = 'CONNECTION_STRING'
     connection = cx_Oracle.connect(database)
     global cur
     cur = connection.cursor()
     last_query = datetime.datetime.now()
     while True:
         try:    
-            SQL = "SELECT MESSAGE, TIMESTAMP FROM (SELECT * FROM LOG_EVENT WHERE LEVEL_STRING IN ('ERROR', 'FATAL') ORDER BY ID DESC) WHERE (TIMESTAMP > TO_TIMESTAMP('" + str(last_query) + "', 'YYYY-MM-DD HH24:MI:SS.FF'))"
+            SQL = "SELECT MESSAGE, TIMESTAMP, LOGGER_NAME FROM (SELECT * FROM LOG_EVENT WHERE LEVEL_STRING IN ('ERROR', 'FATAL') ORDER BY ID DESC) WHERE (TIMESTAMP > TO_TIMESTAMP('" + str(last_query) + "', 'YYYY-MM-DD HH24:MI:SS.FF'))"
             last_query = datetime.datetime.now()
             cur.execute(SQL)
             query_result = cur.fetchall()
             print query_result
-            for error in query_result:
+            for error in reversed(query_result):
                 print "check"
                 message = error[0]
                 timestamp = error[1]
-            sendSlackMesssage(message)
+                logger = error[2]
+                sendSlackMessage(message,timestamp, logger)
         except Exception as e:
             print e
         time.sleep(60)
